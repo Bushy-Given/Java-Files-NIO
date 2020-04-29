eafd0af6d8e54943097bd49bc4b944631a9cf5b3
Bushy <bushy.netshidaulu@gmail.com>
2020-04-29 08:39:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Bushy-Netshidaulu
+ * on 4/14/2020
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> list = new ArrayList<>();
+
+
+    }
+}
---
+++ src/files/Main.java
@@ -0,0 +1,13 @@
+package files;
+
+/**
+ * Created by Bushy-Netshidaulu
+ * on 4/29/2020
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+        
+    }
+}
