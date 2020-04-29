71f073c1e2e99e8503ee942b5e4b97f6ce0f93f7
Bushy <bushy.netshidaulu@gmail.com>
2020-04-29 20:17:00 +0200

    This method iterates over a directory to return a stream whose elements are Path objects that represent the entries of that directory.

--- src/files/Main.java
+++ src/files/Main.java
@@ -1,5 +1,10 @@
 package files;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  * Created by Bushy-Netshidaulu
  * on 4/29/2020
@@ -7,6 +12,12 @@ package files;
 public class Main {
     public static void main(String[] args) {
 
+        final String url = "C:\\Users\\bushy\\Documents\\Git\\projects\\Java-Files-NIO\\temp";
+        try (Stream<Path> list = Files.list(Path.of(url))){
+            list.forEach(System.out::println);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
 
