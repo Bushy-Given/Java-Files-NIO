6f9f28009f197661a62d02007bf839144e1b8f5d
Bushy <bushy.netshidaulu@gmail.com>
2020-05-08 22:42:59 +0200

    resolve(String other) or resolve(Path other) method of java.nio.file.Path used to converts a given path string/Path to a Path and resolves it against this Path in the exact same manner as specified by the resolve method.

--- src/files/_PathsExample.java
+++ src/files/_PathsExample.java
@@ -7,8 +7,10 @@ import java.nio.file.Paths;
  * Created by Bushy-Netshidaulu
  * on 5/8/2020
  */
-public class _Relativize {
+public class _PathsExample {
     public static void main(String[] args) {
+
+        // relativize
         String uri = "C:\\Users\\bushy\\Documents\\Git\\projects\\Java-Files-NIO\\relativize";
         Path path = Paths.get(uri);
         Path passedpath = Paths.get(uri, "equinox\\p2\\core\\cache\\binary");
@@ -17,8 +19,19 @@ public class _Relativize {
         System.out.println("Given Path : " + passedpath);
 
         Path relativice = path.relativize(passedpath);
-
         System.out.println("relativize Path : " + relativice);
+
+        //resolve
+        String passedPath1 = "projects";
+        //takes a string arg
+        Path resolvedPath = path.resolve(passedPath1);
+        System.out.println("ResolvedPath : " + resolvedPath);
+
+        Path passedPath2 = passedpath;
+
+        //takes Path as arg
+        Path resolve = path.resolve(passedPath2);
+        System.out.println("ResolvedPath2 : " + resolve);
     }
 
 }
